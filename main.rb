9c07fce2ddd993f92454e874b4c49199066f06ae
Andrew Babichev <andrew.babichev@gmail.com>
2021-09-08 17:24:33 +0300

    Selenium proxy capability enabled

--- main.rb
+++ main.rb
@@ -4,13 +4,13 @@ chrome_options = {
   args: ["--ignore-certificate-errors"] # Allows to not care about mitmproxy root CA cert
 }
 
-# proxy = Selenium::WebDriver::Proxy.new(http: "#{ENV['HTTP_PROXY_HOST']}:#{ENV['HTTP_PROXY_PORT']}")
+proxy = Selenium::WebDriver::Proxy.new(http: "#{ENV['HTTP_PROXY_HOST']}:#{ENV['HTTP_PROXY_PORT']}")
 
 driver = Selenium::WebDriver.for(
   :remote, url: "http://#{ENV['SELENIUM_HOST']}:#{ENV['SELENIUM_PORT']}/wd/hub",
   capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
     "goog:chromeOptions" => chrome_options,
-  # proxy: proxy
+  proxy: proxy
   )
 )
 
